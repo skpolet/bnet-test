e7b2c30b9abc92297b9e72bebde4b71ae3dbdc95
Sergey <poletvnebesa@yandex.ru>
2019-09-24 20:08:40 +0300

    Initial Commit

---
+++ bnet-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bnet-test
+//
+//  Created by Sergey Mikhailov on 24/09/2019.
+//  Copyright © 2019 Mikhailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
